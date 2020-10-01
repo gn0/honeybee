806a16a55fb012b4539157541d9082454abbb0ac
Gabor Nyeki <gabor.nyeki@alumni.duke.edu>
2020-10-01 17:28:52 -0400

    allow new line + indent for @ commands, too

--- honeybee/comb_to_xlsform/preprocess.py
+++ honeybee/comb_to_xlsform/preprocess.py
@@ -23,26 +23,36 @@ def remove_whitespace(string):
 
 
 def remove_indent(match):
-    return "{}{}{}{}: {}".format(
+    return "{}{}{} {}".format(
         match.group(1),
         match.group(2),
         match.group(3),
-        match.group(4),
-        remove_whitespace(match.group(5)))
+        remove_whitespace(match.group(4)))
+
+
+indent_re = re.compile(
+    r"""# Beginning of file or new line:
+        #
+        (^|\n)
+
+        # Arbitrary amount of indentation, with the first word
+        # not being a keyword:
+        #
+        ([ \t]*(?!if\s|group\s|repeat\s))
+
+        # Question definition or "@" command possibly followed
+        # by whitespace:
+        #
+        (\w+ .*:
+         | @.*)
+        [ ]*
+
+        # Subsequent lines with deeper indent:
+        #
+        ((?:\n\2[ \t]+["\w].*){1,})
+    """,
+    re.VERBOSE)
 
 
 def preprocess_indent(code):
-    return re.sub(
-        # First line: a possibly indented set of letters ending
-        # with a colon.
-        #
-        r"(^|\n)([ \t]*(?!if\s|group\s|repeat\s))(\w+)(.*):[ ]*"
-
-        # Subsequent lines: a set of letters with deeper inden-
-        # tation, possibly occurring multiple times.
-        #
-        + r"((?:\n\2[ \t]+[\"\w].*){1,})",
-
-        remove_indent,
-        code,
-        re.VERBOSE)
+    return indent_re.sub(remove_indent, code)
